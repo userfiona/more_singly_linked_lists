89a8f3f6eac3b26eb46c1bc5c507640ef03748a8
userfiona <fionaramaesela@gmail.com>
2023-02-17 16:56:00 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main . Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = '0';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+	
