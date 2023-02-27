aaad854d440df775e797e280daffff2d3c3c960e
flaviepearl2018@gmail.com <fionaramaesela@gmail.com>
2023-02-27 23:04:52 +0100

    Update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - Entry point
+ * Description: Prints a string to stdout
+ * @str: String data type
+ * Return: str
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
