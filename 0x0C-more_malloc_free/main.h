8894ec8bff6fde2e459e59e048a8e2171ea41246
fiona <fionaramaesela@gmail.com>
2023-03-16 06:10:22 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,11 @@
-#include "main.h"
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
