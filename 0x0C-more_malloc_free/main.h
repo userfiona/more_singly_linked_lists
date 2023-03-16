34c13f7af0dc431417b44ac6472b03df195c22d4
fiona <fionaramaesela@gmail.com>
2023-03-16 04:56:18 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(usigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *realloc(void *ptr, unsigned, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
+#endif
