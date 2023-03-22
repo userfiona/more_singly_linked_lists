b23417472fb489d9595465889c8ebae301352d9b
fiona <fionaramaesela@gmail.com>
2023-03-22 14:18:23 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - excaute function given as parameter on each element of an array
+ *@array: The array
+ *@size: The size of array
+ *@action: a pointer to function to be excauted
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{	
+	if (array == NULL || action == NULL)
+	
+	while (size-- > 0)
+	{
+		action(*array)
+		array++;
+	}
+}
