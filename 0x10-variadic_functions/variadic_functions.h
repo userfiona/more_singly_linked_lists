142d08b613adbd2dc8a747086ff01bae89a6aeae
fiona <fionaramaesela@gmail.com>
2023-03-23 15:39:21 -0700

    0-sum_them_all.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,12 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_
-
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned n, ...);
-void print_strings(const char *separator, const unsigned n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
