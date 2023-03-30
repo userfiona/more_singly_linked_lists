870644538f0dd079ff631bf790333d94cb137dbb
userfiona <fionaramaesela@gmail.com>
2023-03-30 14:25:48 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/2-main.c
+++ 0x12-singly_linked_lists/2-main.c
@@ -11,6 +11,59 @@
 int main(void)
 {
     list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
+    return (0);
+}
+julien@ubuntu:~/0x12. Singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-list_len.c -o b
+julien@ubuntu:~/0x12. Singly linked lists$ ./b 
+-> 2 elements
+julien@ubuntu:~/0x12. Singly linked lists$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x12-singly_linked_lists
+File: 1-list_len.c
+  
+2. Add node
+mandatory
+Write a function that adds a new node at the beginning of a list_t list.
+
+Prototype: list_t *add_node(list_t **head, const char *str);
+Return: the address of the new element, or NULL if it failed
+str needs to be duplicated
+You are allowed to use strdup
+julien@ubuntu:~/0x12. Singly linked lists$ cat 2-main.c
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
 
     head = NULL;
     add_node(&head, "Alexandro");
