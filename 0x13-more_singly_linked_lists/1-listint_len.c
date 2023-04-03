a7e55a2e1ec5c2d9ea83ab972d79ba34cb8c0520
userfiona <fionaramaesela@gmail.com>
2023-04-03 16:19:57 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all the element of linked list
+ * in a linked list
+ * pointer that points to the struct
+ *
+ * Return: number nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n_nodes = 0;
+
+	while (h)
+	{
+		n_nodes++;
+		h = h->next;
+	}
+	return (n_nodes);
+}
