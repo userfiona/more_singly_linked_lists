92cfafe6aa2bb68d60e5517940a0ebabbd8bc442
userfiona <fionaramaesela@gmail.com>
2023-04-04 21:13:33 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all data linked
+ * @head: pointer to the struct
+ *
+ * Return: number of elements
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (0);
+}
