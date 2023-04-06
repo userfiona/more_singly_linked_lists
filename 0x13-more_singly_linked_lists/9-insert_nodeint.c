26d7cf9832b0f16fc80aa05f3fea4e43b09975a6
userfiona <fionaramaesela@gmail.com>
2023-04-06 14:13:20 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,17 +2,17 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to the struct
+set number * @head: pointer to the struct
  * @idx: index of the list to add new node
  * @n: integer in the struct
  * Return: always 0
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *head;
+	listint_t *node, *new_node;
 	unsigned int i;
 
-	if (me.head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 		{
 			index = current->next;
