14573514facbbc5772a5b3141c3083f0a9e2e3c1
userfiona <fionaramaesela@gmail.com>
2023-04-13 14:12:26 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-		file_descriptor = open(filename, O_WRONLY | O_APPEND);
-
-	if (file_descriptor == -1)
+		if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content)
