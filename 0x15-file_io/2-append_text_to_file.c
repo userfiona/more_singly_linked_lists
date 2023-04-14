7a18cbe2b7d1a7dd7b3c0fce2be3e2a8c6aa912e
userfiona <fionaramaesela@gmail.com>
2023-04-14 13:24:05 -0700

    append text at the end

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,36 @@
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file to append text to.
- * @text_content: the text content to be appended to the file.
+ * @filename: name of the file to append to
+ * @text_content: text to append
  *
- * Return: 1 if successful, -1 if the file does not exist or if the operation fails.
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	int num_of_chars;
-	int write_return;
+	int file_descriptor, num_letters, re_value;
 
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	file_descriptor = open(filename, O_RDWR | O_APPEND);
 
 	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		for (num_of_chars = 0; text_content[num_of_chars]; num_of_chars++)
+		for (num_letters = 0; text_content[num_letters]; num_letters++)
 			;
 
-		write_return = write(file_descriptor, text_content, num_of_chars);
+			re_value = write(file_descriptor, text_content, num_letters);
 
-		if (write_return == -1)
+			if (re_value == -1)
 			return (-1);
 	}
 
@@ -36,4 +38,3 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
