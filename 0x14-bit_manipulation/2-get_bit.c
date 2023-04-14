987eb06cf94396e1a4d86bf37ee35b9fe71152f2
userfiona <fionaramaesela@gmail.com>
2023-04-14 09:25:20 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,19 +2,18 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * @num: number which contains the bit
+ * @n: number which contains the bit
  * @index: index at which bit must be found
  *
  * Return: bit (Success) or -1 (error)
  */
-int get_bit(unsigned long int num, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	int my_bit;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-
-		my_bit = (num >> index) & 1;
+		my_bit = (n >> index) & 1;
 
 		return (my_bit);
 }
