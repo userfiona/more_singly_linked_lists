9f77bc8ebdeb8d75308a7352c2c3cdf2fc7ebeee
userfiona <fionaramaesela@gmail.com>
2023-04-16 16:44:17 -0700

    view-updates

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,24 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * set_bit - function that sets the bit of a given index to 1.
+ * set_bit - sets the bit of a given index to 1.
  * @n: pointer to the input number
- * @index: the index of the bit to set to 1
- * 
+ * @index: index of the bit to set to 1
+ *
  * Return: 1 if successful, -1 if failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	// Check if index is greater than maximum bit index (63)
 	if (index > 63)
 		return (-1);
 
-	// Set mask to a 1 shifted to the index position
-	unsigned long int mask = 1 << index;
-
-	// Bitwise OR the mask and the input number to set the bit to 1
-	*n = *n | mask;
-
-	// Return 1 to indicate success
+		unsigned long int bit_mask = 1ul << index;
+	*n |= bit_mask;
 	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,20 +3,20 @@
 
 /**
  * clear_bit - function that clears a bit at a given position in a value
- * @value_ptr: pointer to the value to modify
- * @bit_position: position of the bit to clear
+i * @value_ptr: pointer to the value to modify
+ * @index: index of the bit to clear
  *
  * Description: sets the value of a bit to 0 at a given index
  * Return: 1 if the operation succeeded and -1 if an error occurred
  */
-int clear_bit(unsigned long int *value_ptr, unsigned int bit_position)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	/* Check if the bit position is valid */
-	if (bit_position > (sizeof(*value_ptr) * 8))
+	/* Check if the index is valid */
+	if (index > (sizeof(*n) * 8))
 		return (-1);
 
-	/* Clear the bit at the given position */
-	*value_ptr &= ~(1 << bit_position);
+	/* Clear the bit at the given index */
+	*n &= ~(1 << index);
 
 	/* Operation succeeded */
 	return (1);
