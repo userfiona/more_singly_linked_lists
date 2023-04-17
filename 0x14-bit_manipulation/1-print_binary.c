4f9ff94ea01986bf7d8466158e2c24ae4d94eacb
userfiona <fionaramaesela@gmail.com>
2023-04-16 18:07:27 -0700

    view-updates

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,26 +7,22 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int l;
-    int bit_count;
+	unsigned long int l;
+	int bit_count;
 
- 
-    if (n == 0)
-    {
-        printf("0");
-        return;
-    }
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	for (l = n, bit_count = 0; (l >>= 1) > 0; bit_count++)
+		;
 
-    
-    for (l = n, bit_count = 0; (l >>= 1) > 0; bit_count++)
-        ;
-
-   
-    for (; bit_count >= 0; bit_count--)
-    {
-        if ((n >> bit_count) & 1)
-            printf("1");
-        else
-            printf("0");
-    }
+		for (; bit_count >= 0; bit_count--)
+		{
+		if ((n >> bit_count) & 1)
+			printf("1");
+		else
+			printf("0");
+		}
 }
