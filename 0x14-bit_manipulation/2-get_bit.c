69cfbf58a4c2cb4ecf1a915bde3ed62e7c6ba9b2
userfiona <fionaramaesela@gmail.com>
2023-04-16 17:34:53 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,20 +2,20 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * @num: number which contains the bit
+ * @n: number which contains the bit
  * @bit_index: index at which bit must be found
  *
  * Return: bit (Success) or -1 (error)
  */
-int get_bit(unsigned long int num, unsigned int bit_index)
+int get_bit(unsigned long int n, unsigned int index)
 {
     int bit_value;
 
-    /* If the bit_index is out of range */
-    if (bit_index >= (sizeof(unsigned long int) * 8))
+    /* If the index is out of range */
+    if (index >= (sizeof(unsigned long int) * 8))
         return (-1);
 
-    bit_value = (num >> bit_index) & 1; /* Get the bit value at bit_index */
+    bit_value = (n >> index) & 1; /* Get the bit value at index */
 
     return (bit_value);
 }
