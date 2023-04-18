7b6b513b6eb60ff72d7fee64ecb2687ca3f61e13
userfiona <fionaramaesela@gmail.com>
2023-04-18 12:43:50 -0700

    3-add_nodeint_end.c git push

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,43 +1,65 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list safely
- * @head: pointer to the list
+ * print_listint_safe - Prints a linked list of integers but safely
+ * @head: Pointer to the head node of the list
  *
- * Description: This function prints a linked list, but ensures that an infinite
- * loop is not encountered. It does so by either matching addresses or freeing
- * the head, so that it cannot loop back. It returns the number of nodes in
- * the list.
+ * Description: The function prints the values and addresses of each node in
+ * the list in the order that they appear. It ensures that no infinite loop is
+ * created while printing the list. The function also returns the number of
+ * nodes in the list.
  *
- * Return: the count number of nodes
+ * Return: The number of nodes in the list.
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	size_t index = 0;
-	listint_t const **array; /* an array that stores everything from head */
+	listint_t const **array;
 
-	array = malloc(sizeof(listint_t *) * 1024); /* allocate memory for the array */
+	array = malloc(sizeof(listint_t *) * 1024); // Allocate memory for array
 	if (!array)
-		exit(98);
-	unsigned int i = 0;
-	unsigned int flag = 0; /* a boolean flag */
+		exit(98); // Handle memory allocation error
 
-	while (head != NULL) /* loop through the list */
+	unsigned int i = 0;
+	unsigned int flag = 0;
+
+	while (head != NULL) // Iterate through the list
 	{
-		for (i = 0; i < count; i++) /* loop through the array */
+		for (i = 0; i < count; i++)
 		{
-			if (head == array[i]) /* check for a match between the array values and the head */
+			if (head == array[i]) // Check if the node already exists in the array
 			{
-				flag = 1; /* turn the flag on */
-				index = i; /* make the index equal to the i counter */
+				flag = 1;
+				index = i;
 				break;
 			}
 			else
 				flag = 0;
 		}
 
-		if (flag == 1) /* if we found a match, we break out of the loop */
+		if (flag == 1) // If a loop is found, break out of the loop
 			break;
-		array[count]
+
+		array[count] = head; // Add the current node to the array
+		head = head->next;
+		count++;
+	}
+
+	i = 0;
+	while (i < count) // Print the nodes in the list
+	{
+		printf("[%p] %d\n", (void *)array[i], array[i]->n);
+		i++;
+	}
+
+	if (flag == 1) // If a loop is found, print the node that causes the loop
+	{
+		printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
+	}
+
+	free(array); // Free the memory allocated for the array
+	return (count);
+}
 
