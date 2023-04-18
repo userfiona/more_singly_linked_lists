6739dea0c88f9556975cec42dc677eea103bad3d
userfiona <fionaramaesela@gmail.com>
2023-04-18 12:57:28 -0700

    3-add_nodeint_end.c git push

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,71 +1,59 @@
+#include "lists.h"
+
 /**
- * print_listint_safe - prints a list but safely
- * @head: the head node. pointer
+ * print_listint_safe - prints a linked list of integers, but safely
+ * @head: pointer to the head node
  *
- * Description: dont wanna print a infinite loop
- *     either match addresses or free the head so cant loop back
- * Return: the count number
+ * Description: This function prints a linked list of integers. It keeps
+ * track of the memory addresses of the nodes it has already printed, so
+ * that it does not enter an infinite loop if the list contains a cycle.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    /* A: we make an array that will store everything from head given */
-    size_t node_count = 0;
-    size_t index = 0;
-    listint_t const **node_array;
+	size_t count = 0;        /* number of nodes in the list */
+	size_t index = 0;        /* index of the node where a cycle was detected */
+	listint_t const **array;/* an array to keep track of the nodes */
 
-    node_array = malloc(sizeof(listint_t *) * 1024);
-    if (!node_array)
-        exit(98);
-    unsigned int i = 0;
-    unsigned int flag = 0;
+	array = malloc(sizeof(listint_t *) * 1024);/* allocate memory for the array */
+	if (!array)
+		exit(98);           /* exit if allocation fails */
+	unsigned int i = 0;     /* loop counter */
+	unsigned int flag = 0;  /* flag to indicate if a cycle was detected */
 
-    /* B: while the head is not null, we iterate through */
-    while (head != NULL)
-    {
-        /* C: count is initially 0 and i is 0 so we dont even loop through the first
-         * time, we skip this. we assign array to the head and increment head
-         * then we increase count so when we loop through again, we can go
-         * into the for loop */
-        for (i = 0; i < node_count; i++)
-        {
-            /* D: inside the loop, we check if the head value is equal to the array values
-             * if we found a match then we turn the boolean flag on and break and
-             * we make the index equal to whatever the i counter was */
-            if (head == node_array[i])
-            {
-                flag = 1;
-                index = i;
-                break;
-            }
-            else
-                flag = 0;
-        }
+	while (head != NULL)     /* iterate through the list */
+	{
+		for (i = 0; i < count; i++)/* loop through the array */
+		{
+			if (head == array[i])/* check if the node is already in the array */
+			{
+				flag = 1;       /* set the flag */
+				index = i;      /* save the index */
+				break;          /* break out of the loop */
+			}
+			else
+				flag = 0;
+		}
 
-        /* E: if we found a match, which is a loop, we break out of the while loop */
-        if (flag == 1)
-            break;
-        node_array[node_count] = head;
-        head = head->next;
-        node_count++;
-    }
+		if (flag == 1)  /* if a cycle was detected, break out of the loop */
+			break;
+		array[count] = head;    /* add the node to the array */
+		head = head->next;      /* move to the next node */
+		count++;                /* increment the count */
+	}
 
-    /* F: new loop, we will loop through our array values we copied over from head
-     * and we print out their pointer address and their value. */
-    i = 0;
-    while (i < node_count)
-    {
-        printf("[%p] %d\n", (void *)node_array[i], node_array[i]->n);
-        i++;
-    }
-
-    /* G: if we had an infinite loop we have a flag check of 1 which will
-     * also print out the index which is the last one. */
-    if (flag == 1)
-    {
-        printf("-> [%p] %d\n", (void *)node_array[index], node_array[index]->n);
-    }
-
-    free(node_array);
-    return (node_count);
+	i = 0;
+	while (i < count)           /* loop through the array again */
+	{
+		printf("[%p] %d\n", (void *)array[i], array[i]->n);
+		i++;
+	}
+	if (flag == 1)              /* if a cycle was detected, print the node where it occurs */
+	{
+		printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
+	}
+	free(array);                /* free the array */
+	return (count);             /* return the count */
 }
 
