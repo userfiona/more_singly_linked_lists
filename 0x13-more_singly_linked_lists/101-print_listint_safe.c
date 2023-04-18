09f06b23692297afdb8ad10445082ac9be47dbda
userfiona <fionaramaesela@gmail.com>
2023-04-18 12:53:24 -0700

    3-add_nodeint_end.c git push

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,65 +1,71 @@
-#include "lists.h"
-
 /**
- * print_listint_safe - Prints a linked list of integers but safely
- * @head: Pointer to the head node of the list
+ * print_listint_safe - prints a list but safely
+ * @head: the head node. pointer
  *
- * Description: The function prints the values and addresses of each node in
- * the list in the order that they appear. It ensures that no infinite loop is
- * created while printing the list. The function also returns the number of
- * nodes in the list.
- *
- * Return: The number of nodes in the list.
+ * Description: dont wanna print a infinite loop
+ *     either match addresses or free the head so cant loop back
+ * Return: the count number
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	size_t index = 0;
-	listint_t const **array;
+    /* A: we make an array that will store everything from head given */
+    size_t node_count = 0;
+    size_t index = 0;
+    listint_t const **node_array;
 
-	array = malloc(sizeof(listint_t *) * 1024); // Allocate memory for array
-	if (!array)
-		exit(98); // Handle memory allocation error
+    node_array = malloc(sizeof(listint_t *) * 1024);
+    if (!node_array)
+        exit(98);
+    unsigned int i = 0;
+    unsigned int flag = 0;
 
-	unsigned int i = 0;
-	unsigned int flag = 0;
+    /* B: while the head is not null, we iterate through */
+    while (head != NULL)
+    {
+        /* C: count is initially 0 and i is 0 so we dont even loop through the first
+         * time, we skip this. we assign array to the head and increment head
+         * then we increase count so when we loop through again, we can go
+         * into the for loop */
+        for (i = 0; i < node_count; i++)
+        {
+            /* D: inside the loop, we check if the head value is equal to the array values
+             * if we found a match then we turn the boolean flag on and break and
+             * we make the index equal to whatever the i counter was */
+            if (head == node_array[i])
+            {
+                flag = 1;
+                index = i;
+                break;
+            }
+            else
+                flag = 0;
+        }
 
-	while (head != NULL) // Iterate through the list
-	{
-		for (i = 0; i < count; i++)
-		{
-			if (head == array[i]) // Check if the node already exists in the array
-			{
-				flag = 1;
-				index = i;
-				break;
-			}
-			else
-				flag = 0;
-		}
+        /* E: if we found a match, which is a loop, we break out of the while loop */
+        if (flag == 1)
+            break;
+        node_array[node_count] = head;
+        head = head->next;
+        node_count++;
+    }
 
-		if (flag == 1) // If a loop is found, break out of the loop
-			break;
+    /* F: new loop, we will loop through our array values we copied over from head
+     * and we print out their pointer address and their value. */
+    i = 0;
+    while (i < node_count)
+    {
+        printf("[%p] %d\n", (void *)node_array[i], node_array[i]->n);
+        i++;
+    }
 
-		array[count] = head; // Add the current node to the array
-		head = head->next;
-		count++;
-	}
+    /* G: if we had an infinite loop we have a flag check of 1 which will
+     * also print out the index which is the last one. */
+    if (flag == 1)
+    {
+        printf("-> [%p] %d\n", (void *)node_array[index], node_array[index]->n);
+    }
 
-	i = 0;
-	while (i < count) // Print the nodes in the list
-	{
-		printf("[%p] %d\n", (void *)array[i], array[i]->n);
-		i++;
-	}
-
-	if (flag == 1) // If a loop is found, print the node that causes the loop
-	{
-		printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
-	}
-
-	free(array); // Free the memory allocated for the array
-	return (count);
+    free(node_array);
+    return (node_count);
 }
 
