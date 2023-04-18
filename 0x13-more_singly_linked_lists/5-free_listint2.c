f2ce9ca2f35d5aeeb90f4f0a90cf91617e499b1d
userfiona <fionaramaesela@gmail.com>
2023-04-18 08:30:23 -0700

    recent-updates

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,18 +13,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *current;
 
 	if (head == NULL)
 		return;
 
-		while (*head != NULL)
+		while (head != NULL && *head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		current = *head;
+		*head = current->next;
+		free(current);
 	}
 
 	*head = NULL;
 }
-
