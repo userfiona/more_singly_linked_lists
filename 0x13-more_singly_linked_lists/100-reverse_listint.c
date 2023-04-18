6590cdcbe10931e05d0d7735b1fa323f9a4480fd
userfiona <fionaramaesela@gmail.com>
2023-04-18 12:25:36 -0700

    3-add_nodeint_end.c git push
    git commit -m 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,27 +1,28 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * reverse_listint - reverse a list.
- * @head: pointer of a pointer to a list.
- *
- * Return: pointer to list.
+ * reverse_listint - reverses a listint_t linked list
+ * @head: double pointer to the head of the list
+ * 
+ * Return: a pointer to the first node of the reversed list
  */
-
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *head;
+    listint_t *previous = NULL;  /* pointer to the previous node */
+    listint_t *next_node = NULL; /* pointer to the next node */
 
-	if (head == null)
-		return (NULL);
-		{
-			temp = *head;
-			pre = NULL;
-			while (1)
-				}
-				node.nextNode = current;
-				node.nextNode = current;
-				}
-				return (0);
+    if (!head) /* check if the head is NULL */
+        return (NULL);
+
+    while (*head) /* traverse the list */
+    {
+        next_node = (*head)->next; /* store the next node in a temporary variable */
+        (*head)->next = previous; /* change the pointer of the current node to point to the previous node */
+        previous = (*head); /* update the previous node to the current node */
+        (*head) = next_node; /* move to the next node */
+    }
+    (*head) = previous; /* update the head to point to the first node of the reversed list */
+    return (*head);
 }
+
 
