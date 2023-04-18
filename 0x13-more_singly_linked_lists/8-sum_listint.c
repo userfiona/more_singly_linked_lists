936704ed3cc815a8e98e128212b1fa68d9a417d5
userfiona <fionaramaesela@gmail.com>
2023-04-18 10:53:27 -0700

    recent-updates

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,14 +8,16 @@
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
     if (!head)
-        return (0);
+
+		return (0);
+
     while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-    return (sum);
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
