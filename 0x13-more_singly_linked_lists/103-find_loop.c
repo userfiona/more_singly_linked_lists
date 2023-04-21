8a50661031006c034216381bb5e661ff741c1ae4
userfiona <fionaramaesela@gmail.com>
2023-04-21 11:06:29 -0700

    recent-updates

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,34 +6,29 @@
  *
  * Return: address of the node where the loop starts, or NULL
  */
-listint_t *find_listint_loop(listint_t *head)
+listint_t *find_listint_loop(listint_t *hea)
 {
-	listint_t *slow_ptr = head;
-	listint_t *fast_ptr = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	/* Check if the linked list is empty */
 	if (!head)
 		return (NULL);
 
-	/* Traverse the linked list until we reach the end or find a loop */
-	while (slow_ptr && fast_ptr && fast_ptr->next)
+	while (slow && fast && fast->next)
 	{
-		fast_ptr = fast_ptr->next->next;
-		slow_ptr = slow_ptr->next;
-		/* If we find a loop, reset slow_ptr to head and traverse the list again */
-		if (fast_ptr == slow_ptr)
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
 		{
-			slow_ptr = head;
-			while (slow_ptr != fast_ptr)
+			slow = head;
+			while (slow != fast)
 			{
-				slow_ptr = slow_ptr->next;
-				fast_ptr = fast_ptr->next;
+				slow = slow->next;
+				fast = fast->next;
 			}
-			/* Return the pointer to the node where the loop starts */
-			return (fast_ptr);
+			return (fast);
 		}
 	}
 
-	/* If no loop is found, return NULL */
 	return (NULL);
 }
